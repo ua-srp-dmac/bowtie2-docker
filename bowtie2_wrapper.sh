8a57ef8fc33e9602c54adfcb512ba5b0a2d7c60c
Michelle Yung <myung@email.arizona.edu>
2020-11-12 12:16:53 -0700

    allow grouped & paired reads

--- bowtie2_wrapper.sh
+++ bowtie2_wrapper.sh
@@ -12,41 +12,63 @@ EOF
     exit 0
 }
 
-while getopts "p:s:f:hr:n:" opt; do
+while getopts "p:t:s:f:hr:n:" opt; do
   case ${opt} in
     s)
-     output_file=$OPTARG
-     echo "$output_file"
+      output_file=$OPTARG
+      echo "$output_file"
       ;;
-    p)
-     num_threads=$OPTARG
-     echo "$num_threads"
+    t)
+      num_threads=$OPTARG
+      echo "$num_threads"
       ;;
     f)
-     fastq_file=$OPTARG
-     echo "$fastq_file"
+      if [ -z "$fastq_files" ]
+      then
+        fastq_files=$OPTARG
+      else
+        fastq_files=$read_files','$OPTARG
+      fi
+      echo "$fastq_files"
+      ;;
+    p)
+      if [ -z "$paired_files" ]
+      then
+        paired_files=$OPTARG
+      else
+        paired_files=$paired_files','$OPTARG
+      fi
       ;;
     h)
-     usage
-     exit 1
+      usage
+      exit 1
       ;;    
     r)
-     ref_genome_folder=$OPTARG
-     echo "$ref_genome_folder"
+      ref_genome_folder=$OPTARG
+      echo "$ref_genome_folder"
       ;;
     n)
-     ref_genome_name=$OPTARG
-     echo "$ref_genome_name"
+      ref_genome_name=$OPTARG
+      echo "$ref_genome_name"
       ;;
     \?)
-     echo "Invalid option: -$OPTARG" >&2
-     exit 1
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
       ;;
     :)
-     echo "Option -$OPTARG requires an argument." >&2
-     exit 1
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
       ;;
   esac
 done
 
-bowtie2 -x ${ref_genome_folder}/${ref_genome_name} -U $fastq_file -S ${output_file}.sam -p $num_threads
+if [ -n "$paired_files" ]
+then
+  echo "bowtie2 -x ${ref_genome_folder}/${ref_genome_name} -1 $fastq_files -2 $paired_files -S ${output_file}.sam -p $num_threads"
+  bowtie2 -x ${ref_genome_folder}/${ref_genome_name} -1 $fastq_files -2 $paired_files -S ${output_file}.sam -p $num_threads
+else
+  echo "bowtie2 -x ${ref_genome_folder}/${ref_genome_name} -U $fastq_files -S ${output_file}.sam -p $num_threads"
+  bowtie2 -x ${ref_genome_folder}/${ref_genome_name} -U $fastq_files -S ${output_file}.sam -p $num_threads
+fi
+
+
